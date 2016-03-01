441b22f36d867263d039f0b2b38fc2449003218e
Graham <gthetwong@gmail.com>
2016-02-29 18:27:32 -0800

    same as above

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use('/', routes);
 
 
 var client = new Twitter({
-	consumer_key: process.env.CONSUMER_KEY,
-	consumer_secret: process.env.CONSUMER_SECRET,
-	access_token: process.env.ACCESS_TOKEN,
-	access_token_secret: process.env.ACCESS_SECRET
+	consumer_key: CONSUMER_KEY,
+	consumer_secret: CONSUMER_SECRET,
+	access_token: ACCESS_TOKEN,
+	access_token_secret: ACCESS_SECRET
 });
 
 
