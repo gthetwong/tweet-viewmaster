056bbfbe0e2348be0f00691856ae00f83323cf0d
Graham <gthetwong@gmail.com>
2016-02-29 18:39:15 -0800

    change secrets back to process.env

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use('/', routes);
 
 
 var client = new Twitter({
-	consumer_key: env.CONSUMER_KEY,
-	consumer_secret: env.CONSUMER_SECRET,
-	access_token: env.ACCESS_TOKEN,
-	access_token_secret: env.ACCESS_SECRET
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_SECRET
 });
 
 
